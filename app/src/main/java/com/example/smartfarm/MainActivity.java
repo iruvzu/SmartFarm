e07928d381c646646a085e70680a1805af14a1ef
iruvzu <sunju073127@gmail.com>
2022-11-20 21:30:12 +0900

    [선주] 최종본 전송

--- app/src/main/java/com/example/smartfarm/MainActivity.java
+++ app/src/main/java/com/example/smartfarm/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,6 +28,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
@@ -49,12 +52,19 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> deviceAddressArray;
 
     BluetoothSocket btSocket = null;
-    ConnectedThread connectedThread;
+
+    private OutputStream outputStream = null; //블루투스에 데이터를 출력하기 위한 출력 스트림
+    private InputStream inputStream = null; //블루투스에 데이터를 입력하기 위한 입력 스트림
+    private Thread workerThread = null; //문자열 수신에 사용되는 쓰레드
+    private byte[] readBuffer; //수신된 문자열 저장 버퍼
+    private int readBufferPosition; //버퍼  내 문자 저장 위치
+
 
     int pltmoisture1;
     int pltmoisture2;
     int pltmoisture3;
     int pltbrighter;
+    String[] array = {"0"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -186,6 +196,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 btSocket = createBluetoothSocket(device);
                 btSocket.connect();
+                outputStream = btSocket.getOutputStream();
+                inputStream = btSocket.getInputStream();
             } catch (IOException e) {
                 flag = false;
                 textStatus.setText("connection failed!");
@@ -194,25 +206,26 @@ public class MainActivity extends AppCompatActivity {
 
             if (flag) {
                 textStatus.setText("connected to " + name);
-                connectedThread = new ConnectedThread(btSocket);
-                connectedThread.start();
-
-
+                /*connectedThread = new ConnectedThread(btSocket);
+                connectedThread.start();*/
+                receiveData();
             }
-
         }
     }
 
     public void onClickButtonSend1(View view){
-        if(connectedThread!=null){ connectedThread.write("1"); }
+        sendData("1");
+        Toast.makeText(getApplicationContext(), "1번째 식물에게 물을 줍니다.", Toast.LENGTH_LONG).show();
     }
 
     public void onClickButtonSend2(View view){
-        if(connectedThread!=null){ connectedThread.write("2"); }
+        sendData("2");
+        Toast.makeText(getApplicationContext(), "2번째 식물에게 물을 줍니다.", Toast.LENGTH_LONG).show();
     }
 
     public void onClickButtonSend3(View view){
-        if(connectedThread!=null){ connectedThread.write("3"); }
+        sendData("3");
+        Toast.makeText(getApplicationContext(), "3번째 식물에게 물을 줍니다.", Toast.LENGTH_LONG).show();
     }
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
@@ -232,6 +245,86 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // https://popcorn16.tistory.com/192
+
+    public void receiveData() {
+        final Handler handler = new Handler();
+        //데이터 수신을 위한 버퍼 생성
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+
+        //데이터 수신을 위한 쓰레드 생성
+        workerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        //데이터 수신 확인
+                        int byteAvailable = inputStream.available();
+                        //데이터 수신 된 경우
+                        if (byteAvailable > 0) {
+                            //입력 스트림에서 바이트 단위로 읽어옴
+                            byte[] bytes = new byte[byteAvailable];
+                            inputStream.read(bytes);
+                            //입력 스트림 바이트를 한 바이트씩 읽어옴
+                            for (int i = 0; i < byteAvailable; i++) {
+                                byte tempByte = bytes[i];
+                                //개행문자를 기준으로 받음 (한줄)
+                                if (tempByte == '\n') {
+                                    //readBuffer 배열을 encodeBytes로 복사
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    //인코딩 된 바이트 배열을 문자열로 변환
+                                    final String text = new String(encodedBytes, "UTF-8");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            array = text.split(",");
+
+                                            plantMoisture1.setText(array[0]);
+                                            plantMoisture2.setText(array[1]);
+                                            plantMoisture3.setText(array[2]);
+                                            plantBrighter.setText(array[3]);
+
+                                           /* pltbrighter=Integer.parseInt(array[3]);
+                                            pltmoisture3=Integer.parseInt(array[2]);
+                                            pltmoisture2=Integer.parseInt(array[1]);
+                                            pltmoisture1=Integer.parseInt(array[0]);*/
+
+                                        }
+                                    });
+                                } // 개행문자가 아닐경우
+                                else {
+                                    readBuffer[readBufferPosition++] = tempByte;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+                try {
+                    //1초 마다 받아옴
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        workerThread.start();
+    }
+
+    void sendData(String text) {
+        //문자열에 개행 문자 추가
+        text += "\n";
+        try {
+            //데이터 송신
+            outputStream.write(text.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void plantOnClick(View view)  {
         Intent intent = new Intent(getApplicationContext(), PlantListActivity.class);
